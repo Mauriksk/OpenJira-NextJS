db5aecafb09fdafd660ee06068bc8afa7be1212b
Mauricio Torres <otarksk@gmail.com>
2022-12-06 09:52:49 -0300

    Configuracion de formulario y agrego nueva action

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,37 +1,70 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { AddCircleOutlineOutlined, SaveOutlined } from "@mui/icons-material";
 import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 
 export const NewEntry = () => {
+  const [isAdding, setIsAdding] = useState(false);
+
+  const [inputValue, setInputValue] = useState("");
+  const [touched, setTouched] = useState(false);
+
+  const onTextFieldChanges = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const onSave = () => {
+    if (inputValue.length === 0) {
+      console.log("values", inputValue);
+    }
+  };
+
   return (
-    <Box sx={{marginBottom: 2, paddingX: 2}}>
-      <Button
-        startIcon={<AddCircleOutlineOutlined />}
-        fullWidth
-        variant="outlined"
-      >
-        Agregar Tarea
-      </Button>
+    <Box sx={{ marginBottom: 2, paddingX: 2 }}>
+      {isAdding ? (
+        <>
+          <TextField
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1 }}
+            placeholder="Nueva Entrada"
+            autoFocus
+            multiline
+            label="Nueva Entrada"
+            helperText={inputValue.length <= 0 && touched && "Ingrese un valor"}
+            error={inputValue.length <= 0 && touched}
+            value={inputValue}
+            onChange={onTextFieldChanges}
+            onBlur={() => setTouched(true)}
+          />
 
-      <TextField
-        fullWidth
-        sx={{ marginTop: 2, marginBottom: 1 }}
-        placeholder="Nueva Entrada"
-        autoFocus
-        multiline
-        label="Nueva Entrada"
-        helperText="Ingrese un valor"
-      />
-
-      <Box display={"flex"} justifyContent="space-between">
-        <Button variant="text" color="secondary">
-          Cancelar
+          <Box display={"flex"} justifyContent="space-between">
+            <Button
+              onClick={() => setIsAdding(false)}
+              variant="text"
+              color="secondary"
+            >
+              Cancelar
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              endIcon={<SaveOutlined />}
+              onClick={ onSave }
+            >
+              Guardar
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <Button
+          startIcon={<AddCircleOutlineOutlined />}
+          fullWidth
+          variant="outlined"
+          onClick={() => setIsAdding(true)}
+        >
+          Agregar Tarea
         </Button>
-        <Button variant="outlined" color="secondary" endIcon={<SaveOutlined />}>
-          Guardar
-        </Button>
-      </Box>
+      )}
     </Box>
   );
 };
